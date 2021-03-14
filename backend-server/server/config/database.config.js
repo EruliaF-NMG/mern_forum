b68c4bfd58d19e2f8421e1cf22b47f27b75b0136
Nisal Madusanka (EruliaF) <nisal.madusanka@inovaitsys.com>
2021-03-14 16:31:50 +0530

    add db dump and update env file

--- backend-server/server/config/database.config.js
+++ backend-server/server/config/database.config.js
@@ -2,19 +2,18 @@
  * @Author: Nisal Madusanka(EruliaF)
  * @Date: 2021-03-06 09:29:29
  * @Last Modified by: Nisal Madusanka(EruliaF)
- * @Last Modified time: 2021-03-09 14:17:18
+ * @Last Modified time: 2021-03-14 16:17:02
  */
 
 const dbConfig = {
   connection: process.env.DB_CONNECTION || '',
   host: process.env.DB_HOST || '',
   port: process.env.DB_PORT || 27017,
-  database: process.env.DB_DATABASE || 'myFirstDatabase',
-  userName: process.env.DB_USERNAME || 'nisal',
-  password: process.env.DB_PASSWORD || 'nisal',
+  database: process.env.DB_DATABASE || 'merntest',
+  userName: process.env.DB_USERNAME || '',
+  password: process.env.DB_PASSWORD || '',
 };
 
-//const mongoUri = `mongodb+srv://${dbConfig.userName}:${dbConfig.password}@cluster0.uac76.mongodb.net/${dbConfig.database}?retryWrites=true&w=majority`;
 const mongoUri = 'mongodb://localhost:27017/merntest';
 
 export { dbConfig, mongoUri };
